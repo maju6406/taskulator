471f1192ddfb87ce64d98f01dbe37c02d04d8be7
maju6406 <beersy@gmail.com>
2018-01-16 11:44:17 -0800

    Changing to apply file

--- tasks/run.sh
+++ tasks/run.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 echo module_name:           $PT_module_name
 echo module_version:        $PT_module_version
-echo module_execution_code: $PT_execution_code
+echo module_execution_code: $PT_module_execution_code
 
 puppet module install $PT_module_name
-puppet apply -e $PT_module_execution_code
+echo $puppet_command >/tmp/taskulator.pp 
+puppet apply /tmp/taskulator.pp &>/tmp/taskulator.log
 
 if [ "$PT_postinstall_cleanup" == "yes" ]; then
   puppet module uninstall $PT_module_name
