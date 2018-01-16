d0e98ff8c414b945ccb121d4966863158d876b96
maju6406 <beersy@gmail.com>
2018-01-16 14:31:08 -0800

    Adding debug

--- tasks/run.sh
+++ tasks/run.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
-echo module_names:          $PT_module_names
-echo module_execution_code: $PT_puppet_code
+echo module_names:       $PT_module_names
+echo puppet_code:        $PT_puppet_code
+echo module_postinstall: $PT_postinstall_cleanup
 
 if [ "$PT_postinstall_cleanup" == "" || ["$PT_postinstall_cleanup" == "yes" ]; then
+  echo Uninstall: $PT_postinstall_cleanup
   uninstall_flag = true
 fi
 
 for module_name in $PT_module_names; do
   echo installed $module_name 
-  puppet module install $module_name
+  puppet module install $module_name &>>/tmp/taskulator_install.log
 done 
 
 echo $PT_puppet_code >/tmp/taskulator.pp 
@@ -17,6 +19,6 @@ puppet apply /tmp/taskulator.pp &>/tmp/taskulator.log
 if [ "$uninstall_flag" == true ]; then
   echo Uninstalled modules
   for module_name in $PT_module_names; do
-    puppet module uninstall $module_name
+    puppet module uninstall $module_name &>>/tmp/taskulator_uninstall.log 
   done 
 fi
