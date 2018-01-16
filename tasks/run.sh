692734bd73648bccb68e49d21c4f74d0c25878ef
maju6406 <beersy@gmail.com>
2018-01-16 11:25:30 -0800

    Adding version and echos

--- tasks/run.sh
+++ tasks/run.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
+echo module_name:           $PT_module_name
+echo module_version:        $PT_module_version
+echo module_execution_code: $PT_execution_code
+
 puppet module install $PT_module_name
-puppet apply -e $PT_execution_code
+puppet apply -e $PT_module_execution_code
 
 if [ "$PT_postinstall_cleanup" == "yes" ]; then
   puppet module uninstall $PT_module_name
