baabea1be2b095dad60e43cd709c9f8c559da724
maju6406 <beersy@gmail.com>
2018-01-17 11:58:15 -0800

    Changing location of logs

--- tasks/run.rb
+++ tasks/run.rb
@@ -2,6 +2,7 @@
 
 require 'json'
 require 'open3'
+require 'tmpdir'
 
 def exec(command)
   output, exit_code  = Open3.popen2({}, command, {:err => [:child, :out]}) do  |i, o, w|
@@ -16,24 +17,25 @@ def exec(command)
 end
 
 def install_module(mod)
-  exec("puppet module install #{mod} &>>/tmp/taskulator_install.log")
+  exec("puppet module install #{mod} &>>#{temp}#{File::SEPARATOR}taskulator_install.log")
   puts "#{mod} installed"
 end
 
 def uninstall_module(mod)
-  exec("puppet module uninstall #{mod} &>>/tmp/taskulator_uninstall.log")
+  exec("puppet module uninstall #{mod} &>>#{temp}#{File::SEPARATOR}taskulator_uninstall.log")
 end
 
 def puppet_apply(code)
   File.open("/tmp/taskulator.pp", 'w') { |file| file.write("#{code}") }  
-  exec("puppet apply /tmp/taskulator.pp &>/tmp/taskulator.log")
-  puts "Puppet code executed"
+  exec("puppet apply /tmp/taskulator.pp &>#{temp}#{File::SEPARATOR}taskulator.log")
+  puts "Puppet code executed. Debug output can be found in #{temp}#{File::SEPARATOR}"
 end
 
 params = JSON.parse(STDIN.read)
 puppet_code = params['puppet_code']
 postinstall_cleanup = params['postinstall_cleanup']
 module_names = params['module_names']
+temp = Dir.tmpdir() 
 
 begin
   puts "names:               #{module_names}"
