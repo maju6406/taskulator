58d0f35c2343b1f300f17751a0b118ea6667029b
maju6406 <beersy@gmail.com>
2018-01-17 11:10:55 -0800

    Adding implementation code

--- tasks/run.rb
+++ tasks/run.rb
@@ -16,18 +16,27 @@ def exec(command)
 end
 
 def install_module(mod)
-    puts "module name1: #{mod}"
+  result = exec("puppet module install #{mod} &>>/tmp/taskulator_install.log")
+  puts "install module name: #{mod}"
+  puts result.to_json
+end
+
+def uninstall_module(mod)
+  result = exec("puppet module install #{mod} &>>/tmp/taskulator_install.log")
+  puts "uninstall module name: #{mod}"
+  puts result.to_json
 end
 
 def puppet_apply(code)
-    puts "puppet_code1: #{code}"
+  File.open("/tmp/taskulator.pp", 'w') { |file| file.write("#{code}") }  
+  result = exec("puppet apply /tmp/taskulator.pp &>/tmp/taskulator.log")
+  puts "puppet_code: #{code}"
+  puts result.to_json
 end
 
 params = JSON.parse(STDIN.read)
-#module_names = params['module_names']
 puppet_code = params['puppet_code']
 postinstall_cleanup = params['postinstall_cleanup']
-failonfail = true
 
 begin
   params['module_names'].each do |module_name|
@@ -39,12 +48,13 @@ begin
   end
   puppet_apply(puppet_code)
   puts "postinstall_cleanup: #{postinstall_cleanup}"
-  result = exec("ls")
-  puts result.to_json
-  
-  if failonfail
-    exit result[:exit_code]
-  else
-    exit 0
+  unless postinstall_cleanup == "no"
+    params['module_names'].each do |module_name|
+      begin
+        uninstall_module(module_name)
+      rescue
+        puts "couldn't update file #{module_name}"
+      end  
+    end
   end
 end
